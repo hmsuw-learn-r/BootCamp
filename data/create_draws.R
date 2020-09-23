979796e42e8d19d3721b7f59015780f9b01226f0
zhengp0 <zhengp@uw.edu>
2020-09-23 11:43:48 -0700

    modify the create_draws file to make it more readable

--- data/create_draws.R
+++ data/create_draws.R
@@ -1,5 +1,6 @@
 # Create draws for curve in the simple data set
 # ==============================================================================
+library(readr)
 
 # problem settings
 # ------------------------------------------------------------------------------
@@ -8,18 +9,16 @@ num_groups <- 4
 num_draws <- 20
 
 x_range <- c(0.0, 2.0*pi)
-slope_range <- c(0.0, 2.0)
-intercept_range <- c(-0.5, 0.5)
+s_range <- c(0.0, 2.0)
+i_range <- c(-0.5, 0.5)
 
 # create draws
 # ------------------------------------------------------------------------------
 x <- rep(seq(x_range[1], x_range[2], length.out = num_points),
          times = num_groups)
-slope <- rep(seq(slope_range[1], slope_range[2],
-                 length.out = num_groups),
-             each = num_points)
-intercept <- seq(intercept_range[1], intercept_range[2],
-                 length.out = num_draws)
+s <- rep(seq(s_range[1], s_range[2], length.out = num_groups),
+         each = num_points)
+i <- seq(i_range[1], i_range[2], length.out = num_draws)
 group_id <- rep(1:num_groups, each=num_points)
 
 info = data.frame(
@@ -29,3 +28,10 @@ info = data.frame(
 draws = data.frame(
   outer(x*slope + sin(x), intercept, FUN = "+")
 )
+names(draws) <- paste0("y_", 1:num_draws)
+
+data = cbind(info, draws)
+
+# save data
+# ------------------------------------------------------------------------------
+write_csv(data, "draws.csv")
